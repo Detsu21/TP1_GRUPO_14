ad6bb06a1a87372b99552ac400afc76bdee638e6
Sebacastro14 <sebacastro182@gmail.com>
2026-08-22 15:25:01 -0300

    Agregados constructores en clase Empleado

--- src/ejercicio1/Empleado.java
+++ src/ejercicio1/Empleado.java
@@ -1,11 +1,26 @@
 package ejercicio1;
 
-public class Empleado extends Persona{
-	private int legajo;
-	private String puesto;
+import java.time.LocalDate;
+
+public class Empleado extends Persona {
 	
+	private final int legajo; 
+	private String puesto;
+	private static int contadorLegajo = 1000;
+	
+	// Constructor vacio
 	public Empleado() {
 		super();
+		this.legajo = contadorLegajo;
+		contadorLegajo++;
+	}
+		
+	// Segundo constructor con parámetros
+	public Empleado(String dni, String nombre, String apellido, LocalDate fechaNacimiento, String genero, String direccion, String telefono, String email, String puesto) {
+		super(dni, nombre, apellido, fechaNacimiento, genero, direccion, telefono, email);
+		this.puesto = puesto;
+		this.legajo = contadorLegajo;
+		contadorLegajo++;
 	}
 	
 	public int getLegajo() {
